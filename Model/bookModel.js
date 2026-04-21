24edb622f0a2781013eee2ec72c399eb36b6261f
kashamrokaya-cyber <kashamrokaya18@gmail.com>
2026-04-21 14:07:12 +0545

    all apis created CRUD

---
+++ Model/bookModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bookSchema = new Schema({
+    bookName: {
+        type: String,
+        unique: true
+    },
+    bookPrice: {
+        type: String
+    },
+    isbnNumber: {
+        type: Number
+    },
+    authorName: {
+        type: String
+    },
+    publishedAt: {
+        type: String
+    }
+})
+
+
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
--- app.js
+++ app.js
@@ -1,26 +1,97 @@
 const express = require('express');
 const app = express();
+const ConnectionString = require('./database/index')
+app.use(express.json());
+const Book = require('./Model/bookModel')
 
 
+ConnectionString();
 
 
+//Add or Create book
+app.post('/book', async (req, res) => {
+    // console.log(req.body.bookName);
+    // console.log(req.body.authorName);
 
+    const { bookName, authorName, bookPrice, isbnNumber, publishedAt } = req.body;
+    await Book.create({
+        bookName: bookName,
+        authorName: authorName,
+        bookPrice: bookPrice,
+        isbnNumber: isbnNumber,
+        publishedAt: publishedAt
+    })
+    res.status(200).json({
+        message: "Book is successfully created"
+    })
 
-
-
-app.get('/',(req,res)=>{
-    
-    res.send("Bye kushal")
 })
+//fetch single book
+app.get('/book/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const oneBook = await Book.findById(id);
+        if (!oneBook) {
+            res.status(404).json({
+                message: "no book is avalable"
+
+            })
+
+
+        } else {
+
+            res.status(200).json({
+                message: "single book is available",
+                data: oneBook
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "something went wrong"
+        });
+    };
+});
+
+
+//fetch all book
+app.get('/', async (req, res) => {
+
+    const Books = await Book.find();
+    res.status(200).json({
+        message: "Fetched all successfully",
+        data: Books
+    });
+});
+//delete book 
+app.delete('/book/:id', async (req, res) => {
+    const id = req.params.id;
+    await Book.findByIdAndDelete(id);
+    res.status(200).json({
+        message: "successfully deleted"
+    });
+});
+
+//update books
+app.patch('/book/:id', async (req, res) => {
+    const id = req.params.id;
+    const { bookName, authorName, bookPrice, isbnNumber, publishedAt } = req.body;
+    await Book.findByIdAndUpdate(id, {
+        bookName: bookName,
+        authorName: authorName,
+        bookPrice: bookPrice,
+        isbnNumber: isbnNumber,
+        publishedAt: publishedAt
+    });
+    res.status(200).json({
+        message: "updated successfully..."
+    });
+});
 
 
 
 
 
-
-
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("App is listening...")
 })
 
