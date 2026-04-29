19780a09b6b42b77757c61cdb243e0c9a91af324
kashamrokaya-cyber <kashamrokaya18@gmail.com>
2026-04-29 13:50:54 +0545

    index.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const ConnectionString = require('./database/index')
+const ConnectionString = require('./database')
 app.use(express.json());
 const Book = require('./Model/bookModel')
 const path = require('path')
